8daf5409bf3146e31245894b585e1fb13a1b5e4f
Gilbert Asare <gai.dev.official@gmail.com>
2026-07-21 16:53:57 +0000

    fix: resolve undefined media_disk function

--- app/Models/User.php
+++ app/Models/User.php
@@ -154,11 +154,11 @@ class User extends Authenticatable
         $mediaFiles = $this->mediaFiles;
         $totalSize = 0;
         foreach ($mediaFiles as $file) {
-            if ($file->recipient_image && \Illuminate\Support\Facades\Storage::disk(media_disk())->exists($file->recipient_image)) {
-                $totalSize += \Illuminate\Support\Facades\Storage::disk(media_disk())->size($file->recipient_image);
+            if ($file->recipient_image && \Illuminate\Support\Facades\Storage::disk(config('filesystems.media_disk'))->exists($file->recipient_image)) {
+                $totalSize += \Illuminate\Support\Facades\Storage::disk(config('filesystems.media_disk'))->size($file->recipient_image);
             }
-            if ($file->background_music && \Illuminate\Support\Facades\Storage::disk(media_disk())->exists($file->background_music)) {
-                $totalSize += \Illuminate\Support\Facades\Storage::disk(media_disk())->size($file->background_music);
+            if ($file->background_music && \Illuminate\Support\Facades\Storage::disk(config('filesystems.media_disk'))->exists($file->background_music)) {
+                $totalSize += \Illuminate\Support\Facades\Storage::disk(config('filesystems.media_disk'))->size($file->background_music);
             }
         }
         return $totalSize;
